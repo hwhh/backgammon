67733382da1b90af5fb8c42513592a9119c6e9f1
Henry Hargreaves <hwhh@st-andrews.ac.uk>
2019-09-26 22:52:52 +0100

    adding die functionality

--- FrontEnd/GUI.py
+++ FrontEnd/GUI.py
@@ -1,43 +1,70 @@
 import math
 
-import pygame
+import pygame, random, time
+from pygame import font
 
-WHITE = (200, 200, 150)
+WHITE = (255, 255, 255)
+WOOD = (200, 200, 150)
 BLACK = (0, 0, 0)
 
 
-# TODO only 10 rows supported atm screen_to_pos not working
-
-# 6 is the max -> up to 15 but then stack?
-
-
-def draw_counters(board):
-    for piece in board.pieces:  # TODO rename pieces to spikes or something better
-        continue
+# TODO migrate this to a class?
+def calculate_spacing(board, x):
+    if len(board.pieces[x]) <= 5:
+        return 50
+    else:
+        return int(round(50 - (len(board.pieces[x]) * 1.5)))
 
 
-def pos_to_screen(board_location, distance):  # TODO add max y limits for counters
+def pos_to_screen(board_location, distance):
     if board_location[0] < 6:
-        return (-15 + (90 * (board_location[0] + 1))), (distance * (board_location[1] + 1))
+        x, y = (-15 + (90 * (board_location[0] + 1))), (distance * (board_location[1] + 1))
     elif 6 <= board_location[0] < 12:
-        return (45 + (90 * (board_location[0] + 1))), (distance * (board_location[1] + 1))
+        x, y = (45 + (90 * (board_location[0] + 1))), (distance * (board_location[1] + 1))
     elif 12 <= board_location[0] < 17:
-        return (-15 + (90 * (board_location[0] - 11))), (765 - (distance * (board_location[1])))
-    elif board_location[0] > 17:
-        return (45 + (90 * (board_location[0] - 11))), (765 - (distance * (board_location[1])))
+        x, y = (-15 + (90 * (board_location[0] - 11))), (765 - (distance * (board_location[1])))
+    else:
+        x, y = (45 + (90 * (board_location[0] - 11))), (765 - (distance * (board_location[1])))
+
+    # TODO draw number of counters stacked at this point
+    # TODO fix bug with distance shifting the counter off the edge of board
+
+    if board_location[0] <= 11 and y > 400:
+        y = 400
+    if board_location[0] > 11 and y < 420:
+        y = 420
+    return x, y
 
 
-def screen_to_pos(event):
-    if event.pos[0] < 575 and event.pos[1] > 410:
-        return math.floor((event.pos[0] / 90)) + 12
-    elif event.pos[0] < 575 and event.pos[1] <= 410:
-        return math.floor((event.pos[0] / 90))
-    elif event.pos[0] >= 575 and event.pos[1] > 410:
-        return math.floor(((event.pos[0] - 80) / 90)) + 12
-    elif event.pos[0] >= 575 and event.pos[1] <= 410:
-        return math.floor(((event.pos[0] - 80) / 90))
+def screen_to_pos(event, board):
+    if 575 <= event.pos[0] <= 622 and 790 <= event.pos[1] <= 820:
+        return -1
+    else:
+        if event.pos[0] < 575 and event.pos[1] > 410:
+            x = math.floor((event.pos[0] / 90)) + 12
+        elif event.pos[0] < 575 and event.pos[1] <= 410:
+            x = math.floor((event.pos[0] / 90))
+        elif event.pos[0] >= 575 and event.pos[1] > 410:
+            x = math.floor(((event.pos[0] - 80) / 90)) + 12
+        else:
+            x = math.floor(((event.pos[0] - 80) / 90))
+
+        distance = calculate_spacing(board, x)
+        if x <= 11 and event.pos[1] < 410:
+            return (distance * len(board.pieces[x]) - 25) <= event.pos[1] <= (distance * len(board.pieces[x]) + 25)
+        else:
+            return (790 - (distance * len(board.pieces[x]))) <= event.pos[1] <= (
+                    840 - (distance * len(board.pieces[x])))
 
 
+def roll_dice(display):
+    die1, die2 = random.choice([1, 2, 3, 4, 5, 6]), random.choice([1, 2, 3, 4, 5, 6])
+    font = pygame.font.SysFont('Arial', 25)
+    pygame.draw.rect(display, WHITE, (540, 420, 40, 40))
+    display.blit(font.render(str(die1), True, (0, 0, 0)), (555, 435))
+    pygame.draw.rect(display, WHITE, (600, 420, 40, 40))
+    display.blit(font.render(str(die2), True, (0, 0, 0)), (615, 435))
+    return die1, die2
 
 
 def run_game(board):
@@ -47,47 +74,29 @@ def run_game(board):
 
     background = pygame.transform.smoothscale(pygame.image.load('./backgammon/Assets/board.png').convert(), res)
 
-    counters = []
     for piece in board.get_pieces():
-        if len(board.pieces[piece.loc[0]]) <= 5:
-            distance = 50
-        else:
-            distance = int(round(50 - (len(board.pieces[piece.loc[0]]) * 1.5)))
-        pygame.draw.circle(background, WHITE if piece.colour == 'w' else BLACK, pos_to_screen(piece.loc, distance), 25)
-
-    #
-
-    # for i in range(15):
-    #     pygame.draw.circle(background, BLACK, pos_to_screen((3, i)), 30)
-    #
-    # a = 0
-    # clock = pygame.time.Clock()
-
-    # display.blit(background, (0, 0))
-    # pygame.display.update()
-    # while True:
-    #     clock.tick(60)
-    #     rect = (a, 8, 339, 205)
-    #     display.blit(background, rect, rect)  # draw the needed part of the background
-    #     pygame.display.update(rect)  # update the changed area
-    #     a += 10
-    #     rect = (a, 8, 339, 205)
-    #     pygame.draw.rect(display, (255, 0, 0), rect)
-    #     pygame.display.update(rect)  # update the changed area
-
+        location = pos_to_screen(piece.loc, calculate_spacing(board, piece.loc[0]))
+        pygame.draw.circle(background, WOOD if piece.colour == 'w' else BLACK, location, 25)
     clock = pygame.time.Clock()
-
     display.blit(background, (0, 0))
+    pygame.display.update()
+
+    font = pygame.font.SysFont('Arial', 25)
+    pygame.draw.rect(display, WHITE, (575, 790, 47, 30))
+    display.blit(font.render('Roll', True, (0, 0, 0)), (583, 800))
     pygame.display.update()
 
     run = True  # todo get the game state
     while run:
-        clock.tick(60)
+        clock.tick(120)
+        pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    index = screen_to_pos(event)
+                    index = screen_to_pos(event, board)
                     print(index)
-                    # print(board.move((x, y), None))
+                    if index == -1:
+                        roll_dice(display)
+
             if event.type == pygame.QUIT:
                 run = False
